c6fe6d131abcc8446e16c0581333829b6ca1462b
ivelin1936 <ivelin_1936@abv.bg>
2022-02-20 18:23:10 +0200

    Added third docker compose ex - separat sensitive data from the docker compose yaml file into the .env

---
+++ docker/Docker Compose/ex3/init-compose.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "### Checking validity of the docker configuration..."
+docker compose config
+
+echo "### Initiating the build of the image..."
+docker compose build
+
+echo "### Checking hte list of images..."
+docker image ls
+
+echo "### Starting the service/s with attached session, to the session of the running container.."
+echo "## For detached mode use '-d'"
+docker compose up -d
+
+echo "### Listing docker compose objects.."
+docker compose ps
+
+

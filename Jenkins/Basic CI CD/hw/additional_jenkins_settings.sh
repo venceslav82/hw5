0d885d195f6a3a6ab40975fe694d21ef2f44e32b
ivelin1936 <ivelin_1936@abv.bg>
2022-03-04 14:23:54 +0200

    improved

--- Jenkins/Basic CI CD/hw/additional_jenkins_settings.sh
+++ Jenkins/Basic CI CD/hw/additional_jenkins_settings.sh
@@ -10,20 +10,27 @@ echo "### Change jenkin's shell from '/bin/false' to '/bin/bash'"
 JENKINS_USER=$(cat /etc/passwd | grep jenkins)
 [[ "$JENKINS_USER" == *bin/false ]] && sudo usermod -s /bin/bash jenkins
 
-#TODO...
-echo "### Entering into bin/bash as jenkins..."
-sudo su -s /bin/bash jenkins
-
-echo "### Generating an SSH key..."
-echo | ssh-keygen -t rsa -m PEM -P ''
-
-echo "Copy the key to localhost..."
-sshpass -p "Password1" ssh-copy-id jenkins@localhost
-
-#TODO ..... page 5
-
 echo "### Adding the jenkins user to the sudoers list..."
 sudo sh -c "echo \"jenkins ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
+
+
+
+#TODO...
+#echo "### Entering into bin/bash as jenkins..."
+#sudo su -s /bin/bash jenkins
+
+#echo "### Generating an SSH key..."
+#echo | ssh-keygen -t rsa -m PEM -P ''
+
+#echo "Copy the key to localhost..."
+#sshpass -p "Password1" ssh-copy-id jenkins@localhost
+#TODO ..... page 5
+
+
+
+echo "### Disabling Jenkins security to prevent unlocking..."
+sudo sed -i 's/<useSecurity>true<\/useSecurity>/<useSecurity>false<\/useSecurity>/g' /var/lib/jenkins/config.xml
+sudo ex +g/useSecurity/d +g/authorizationStrategy/d -scwq /var/lib/jenkins/config.xml
 
 echo "### Restarting jenkins service..."
 sudo systemctl restart jenkins

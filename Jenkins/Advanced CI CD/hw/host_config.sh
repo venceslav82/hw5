84bdbeb3e3c7d9af9986132831e1cc53a82e123a
venceslav82 <93188285+venceslav82@users.noreply.github.com>
2022-03-16 14:25:50 +0200

    Update host_config.sh

--- Jenkins/Advanced CI CD/hw/host_config.sh
+++ Jenkins/Advanced CI CD/hw/host_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "* Add hosts ..."
-echo "192.168.56.11 jenkins.m5.hw jenkins" >> /etc/hosts
-echo "192.168.56.12 docker.m5.hw docker" >> /etc/hosts
+echo "192.168.99.11 jenkins.m5.hw jenkins" >> /etc/hosts
+echo "192.168.99.12 docker.m5.hw docker" >> /etc/hosts
 
 echo "* Install Additional Packages ..."
 sudo dnf install -y jq tree git nano sshpass
